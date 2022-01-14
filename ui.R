763a66d463614cc3960eb4e8360c9d4a8f500c7a
zubprem <90324289+zubprem@users.noreply.github.com>
2022-01-15 00:56:52 +0530

    Update ui.R

--- ui.R
+++ ui.R
@@ -5,7 +5,14 @@ library(shiny)
 shinyUI(fluidPage(
 
     # Application title
-    titlePanel("Swiss Data"),
+    titlePanel("Swiss Data Basic Exploration Plot"),
+    h3("The plot tab contains a plot of Fertility vs chosen input 'x' variable"),
+    selectInput("variable", "Variable:",
+                c("Agriculture" = "Agriculture",
+                  "Examination" = "Examination",
+                  "Education" = "Education",
+                  "Catholic" = "Catholic",
+                  "Infant Mortality" = "Infant.Mortality")),
     checkboxInput("show_values", "Show/Hide Actual Slope & Intercept values", value = FALSE),
     
     sidebarLayout(
@@ -18,7 +25,14 @@ shinyUI(fluidPage(
 
         # Show a the required plot
         mainPanel(
-            plotOutput("plot1", brush = brushOpts(id = "brush1"))
+            tabsetPanel(type = "tabs", 
+                        tabPanel("Documentation", br(), h3("Choose desired x variable from drop down 
+                                                           and select data points using brush for linear model in the
+                                                           plot tab.
+                                                           Complete details of the dataset can be found from offical
+                                                           documentation.
+                                                           ")),
+                        tabPanel("Plot", br(), plotOutput("plot1", brush = brushOpts(id = "brush1"))))
         )
     )
 ))
